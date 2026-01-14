8c23925345e1bc74124426cdd2b283a40a9ce0a1
Geraldi Aditya <dev.geraldi@gmail.com>
2026-01-14 20:42:52 +0700

    feat(cinema): add create/update/deleted at for tracking

--- internal/modules/cinema/domain/entity.go
+++ internal/modules/cinema/domain/entity.go
@@ -1,19 +1,31 @@
 package domain
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type Cinema struct {
-	ID        int64   `gorm:"primaryKey" json:"id"`
-	Name      string  `gorm:"not null;type:varchar(100)" json:"name"`
-	City      string  `gorm:"not null;type:varchar(50)" json:"city"`
-	Address   string  `gorm:"type:text" json:"address"`
-	BasePrice float64 `gorm:"not null;type:decimal(10,2);default:50000" json:"base_price"`
+	ID        int64          `gorm:"primaryKey" json:"id"`
+	Name      string         `gorm:"not null;type:varchar(100)" json:"name"`
+	City      string         `gorm:"not null;type:varchar(50)" json:"city"`
+	Address   string         `gorm:"type:text" json:"address"`
+	BasePrice float64        `gorm:"not null;type:decimal(10,2);default:50000" json:"base_price"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
 }
 
 type Theater struct {
-	ID       int64  `gorm:"primaryKey" json:"id"`
-	CinemaID int64  `gorm:"not null" json:"cinema_id"`
-	Cinema   Cinema `gorm:"foreignKey:CinemaID" json:"cinema"`
-	Name     string `gorm:"not null;type:varchar(50)" json:"name"` // e.g. "Studio 1", "IMAX"
-	Type     string `gorm:"type:varchar(20)" json:"type"`          // Regular, IMAX, Premiere
+	ID        int64          `gorm:"primaryKey" json:"id"`
+	CinemaID  int64          `gorm:"not null" json:"cinema_id"`
+	Cinema    Cinema         `gorm:"foreignKey:CinemaID" json:"cinema"`
+	Name      string         `gorm:"not null;type:varchar(50)" json:"name"` // e.g. "Studio 1", "IMAX"
+	Type      string         `gorm:"type:varchar(20)" json:"type"`          // Regular, IMAX, Premiere
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
 }
 
 type CinemaRepository interface {
