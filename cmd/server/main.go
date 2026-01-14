06cf2df3ca496cb7eba3b81c4547e48e751dee4f
Geraldi Aditya <dev.geraldi@gmail.com>
2026-01-14 20:43:54 +0700

    feat(app main): add soft shutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/geraldiaditya/ratix-backend/internal/config"
@@ -194,9 +198,32 @@ func main() {
 		cinemaHandler.RegisterRoutes(app)
 
 		// 5. Start Server
+		// 5. Start Server (Graceful Shutdown)
 		log.Printf("Starting server on port %s", cfg.ServerPort)
-		if err := app.Listen(":" + cfg.ServerPort); err != nil {
-			log.Fatalf("Server failed: %v", err)
+
+		go func() {
+			if err := app.Listen(":" + cfg.ServerPort); err != nil {
+				log.Fatalf("Server failed: %v", err)
+			}
+		}()
+
+		// Wait for interrupt signal to gracefully shutdown the server with
+		// a timeout of 5 seconds.
+		quit := make(chan os.Signal, 1)
+		// kill (no param) default send syscall.SIGTERM
+		// kill -2 is syscall.SIGINT
+		// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		<-quit
+		log.Println("Shutting down server...")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if err := app.ShutdownWithContext(ctx); err != nil {
+			log.Fatalf("Server forced to shutdown: %v", err)
 		}
+
+		log.Println("Server exiting")
 	}
 }
